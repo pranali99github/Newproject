0a3350c53e3f63dc8ffd51bdf8909ba2a61bfdf6
pranali99github <pranaliwankhede927@gmail.com>
2023-03-17 13:48:38 +0530

    done

---
+++ Collectiondemo/src/com/demo/ArrayListdemo.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListdemo {
+
+	public static void main(String[] args) {
+		
+		List<String> list= Arrays.asList("pranali","rohan","ram");
+            
+		System.out.println(list);
+	}
+
+}
---
+++ collectiondemoo/src/com/collectiondemoo/arraylistdemo.java
@@ -0,0 +1,15 @@
+package com.collectiondemoo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class arraylistdemo {
+
+	public static void main(String[] args) {
+		
+		List<String> list =Arrays.asList("ajay","vijay","ram");
+		
+		System.out.println(list);
+	}
+
+}
